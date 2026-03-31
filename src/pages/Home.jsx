cde664fdb18a6286f180e5ce40c20aae1ff52294
iRhyke <256hayato@gmail.com>
2026-03-31 21:23:11 +0900

    main:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import HeroSection from "../components/home/HeroSection";
 import FeaturesSection from "../components/home/FeaturesSection";
-import simulatorPreview from "../components/home/simulatorPreview";
-import pricingPreview from "../components/home/pricingPreview";
+import SimulatorPreview from "../components/home/simulatorPreview";
+import PricingPreview from "../components/home/pricingPreview";
 import FlowSection from "../components/home/FlowSection";
-import accessPreview from "../components/home/accessPreview";
+import AccessPreview from "../components/home/accessPreview";
 import LINESection from "../components/home/LINESection";
 import CTASection from "../components/home/CTASection";
 
@@ -13,10 +13,10 @@ export default function Home() {
     <div>
       <HeroSection />
       <FeaturesSection />
-      <simulatorPreview />
-      <pricingPreview />
+      <SimulatorPreview />
+      <PricingPreview />
       <FlowSection />
-      <accessPreview />
+      <AccessPreview />
       <LINESection />
       <CTASection />
     </div>
