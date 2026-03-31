e6d1e9ef5cfc4ac9c9ca56360f07d3dbcc8e4014
iRhyke <256hayato@gmail.com>
2026-03-31 21:30:25 +0900

    main: 大文字小文字のエラー箇所修正

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import HeroSection from "../components/home/HeroSection";
 import FeaturesSection from "../components/home/FeaturesSection";
-import SimulatorPreview from "../components/home/simulatorPreview";
-import PricingPreview from "../components/home/pricingPreview";
+import SimulatorPreview from "../components/home/SimulatorPreview";
+import PricingPreview from "../components/home/PricingPreview";
 import FlowSection from "../components/home/FlowSection";
-import AccessPreview from "../components/home/accessPreview";
+import AccessPreview from "../components/home/AccessPreview";
 import LINESection from "../components/home/LINESection";
 import CTASection from "../components/home/CTASection";
 
